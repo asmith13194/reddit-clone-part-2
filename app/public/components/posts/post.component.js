6badbfbf1857307427ca69cb3298ea6e8c0c5cb3
Adam Smith <asmith13194@gmail.com>
2017-07-07 20:08:31 -0600

    fix delete button

--- app/public/components/posts/post.component.js
+++ app/public/components/posts/post.component.js
@@ -5,7 +5,7 @@
     controller: controller
   });
 
-  controller.$inject = ['$http']
+  controller.$inject = ['$http'];
   function controller($http){
     const vm = this;
     vm.master = [];
@@ -32,29 +32,8 @@
       posty.editPostToggle = true;
     };
 
-    vm.deletePost = function(post){
-      $http.delete('/api/posts/'+post.id).then(()=>{
-        let postbox = vm.master.indexOf(post);
-        vm.master.splice(postbox,postbox+1);
-      });
-    };
-
-    vm.deleteComment = function(post,comment){
-      $http.delete('/api/posts/'+post.id+'/comments/'+comment.id).then(()=>{
-        let postbox = vm.master.indexOf(post);
-        let commentbox = vm.master[postbox].comments.indexOf(comment);
-        vm.master[postbox].comments.splice(commentbox,commentbox+1);
-      });
-    };
-
-    vm.updateComment = function(post,comment){
-      $http.patch('/api/posts/'+post.id+'/comments/'+comment.id,{content:comment.content}).then(response=>{
-      });
-    };
-
     vm.createPost = function(post){
       vm.newPostToggle = false;
-      // post.comments = [];
       post.vote_count = 0;
       post.showCommentsToggle = false;
       post.editPostToggle = false;
@@ -77,6 +56,15 @@
       });
     };
 
+    vm.deletePost = function(post){
+      $http.delete('/api/posts/'+post.id).then(()=>{
+        let postbox = vm.master.indexOf(post);
+        console.log(vm.master);
+        vm.master.splice(postbox,1);
+        console.log(vm.master);
+      });
+    };
+
     vm.showComments = function(posty){
       if(posty.showCommentsToggle === true){
         posty.showCommentsToggle = false;
@@ -93,6 +81,19 @@
       let postbox = vm.master.indexOf(post);
       $http.post('/api/posts/'+post.id+'/comments',{content:comment}).then(response=>{
         vm.master[postbox].comments.push(response.data);
+      });
+    };
+
+    vm.updateComment = function(post,comment){
+      $http.patch('/api/posts/'+post.id+'/comments/'+comment.id,{content:comment.content}).then(response=>{
+      });
+    };
+
+    vm.deleteComment = function(post,comment){
+      $http.delete('/api/posts/'+post.id+'/comments/'+comment.id).then(()=>{
+        let postbox = vm.master.indexOf(post);
+        let commentbox = vm.master[postbox].comments.indexOf(comment);
+        vm.master[postbox].comments.splice(commentbox,commentbox+1);
       });
     };
 
@@ -119,43 +120,6 @@
     vm.$onInit = function(){
       $http.get('/api/posts').then(function (response) {
         vm.master = (response.data);
-        // vm.master.push({
-        //   title: 'Suhh',
-        //   body: 'Suhh Dude',
-        //   author: 'Suhh Dude',
-        //   image_url: "https://www.mememaker.net/static/images/memes/4400130.jpg",
-        //   comments: [],
-        //   vote_count: 0,
-        //   fullDate: new Date().toUTCString(),
-        //   day: 30,
-        //   date: 'Fri',
-        //   month: 'Jun',
-        //   year: 2017
-        // },{
-        //   title: 'Reddit Clone',
-        //   body: 'Blackjack and Hookers',
-        //   author: 'Bender',
-        //   image_url: "http://s2.quickmeme.com/img/e7/e72b69c1ea95f7bed4bf9cdbdc5d4a35a28b3a23e80d12256d5b28633dc799b6.jpg",
-        //   comments: [],
-        //   vote_count: 0,
-        //   fullDate: new Date().toUTCString(),
-        //   day: 30,
-        //   date: 'Fri',
-        //   month: 'Jun',
-        //   year: 2017
-        // },{
-        //   title: 'Pepe Trump',
-        //   body: 'Make Reddit Meme Again',
-        //   author: 'Pepe',
-        //   image_url: "https://cdn.vox-cdn.com/uploads/chorus_asset/file/6666909/pepe-trump.0.png",
-        //   comments: [],
-        //   vote_count: 0,
-        //   fullDate: new Date().toUTCString(),
-        //   day: 30,
-        //   date: 'Fri',
-        //   month: 'Jun',
-        //   year: 2017
-        // });
       });
     };
 
